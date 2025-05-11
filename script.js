44dca482ab2a3aa851261991ad0849c1f917eb56
digitalsuni-cloud <digitalsuni@hotmail.com>
2025-05-11 10:33:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -834,8 +834,10 @@ function downloadText(textareaId, filename) {
   const text = document.getElementById(textareaId).value;
   const blob = new Blob([text], { type: 'text/plain' });
   const link = document.createElement("a");
+  const bookNameInput = document.getElementById('bookName');
+  const bookName = bookNameInput.value.trim();
   link.href = URL.createObjectURL(blob);
-  link.download = filename;
+  link.download = bookName;
   link.click();
 }
 
